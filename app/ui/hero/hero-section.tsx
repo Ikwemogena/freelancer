798ab6b382d63df0f14b7b264a5533c43232859a
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-03-27 18:04:01 +0100

    updated hero section content.

--- app/ui/hero/hero-section.tsx
+++ app/ui/hero/hero-section.tsx
@@ -4,15 +4,16 @@ import NavBar from '../nav-bar'
 export default function HeroSection() {
   return (
     <>
+      {/* <NavBar /> */}
       <section className='hero__section'>
         <div className='black-overlay'></div>
         <div className='hero__section-content'>
-          {/* <h1>create</h1>
+          <h1>create</h1>
           <h1>innovate</h1>
-          <h1>connect</h1> */}
-          <p>create</p>
+          <h1>connect</h1>
+          {/* <p>create</p>
           <p>innovate</p>
-          <p>connect</p>
+          <p>connect</p> */}
         </div>
 
       </section>
