fda05bc0421af306395e9bb483d1b23757ac4e86
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-03-27 18:11:00 +0100

    fix: typo in testimonial text

--- app/ui/testimonial/testimonial.tsx
+++ app/ui/testimonial/testimonial.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Testimonial() {
     return (
         <section className='testimonial'>
+            {/* <div className='black-overlay'></div> */}
             <div className="testimonial__text">
-                <p>“The environment at Freelancer is super inclusive. I love coming here knowing i’m going to have a fun, productive day”</p>
+                <p>“The environment at Freelancer is super inclusive. I love coming here knowing I’m going to have a fun, productive day”</p>
                 <h3>Courtney Henry</h3>
             </div>
         </section>
