3b1177970dc9ffc73a26b98312020d3033228c73
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-03-28 16:04:29 +0100

    Add animation to audience cards

--- app/ui/audience/audience-section.tsx
+++ app/ui/audience/audience-section.tsx
@@ -1,23 +1,44 @@
+"use client"
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { useGSAP } from '@gsap/react'
 import Image from 'next/image'
-import React from 'react'
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(useGSAP)
 
 export default function AudienceSection() {
+
+    useGSAP(() => {
+        gsap.from('.audience__content-cards', {
+            delay: 0.5,
+            duration: 1,
+            opacity: 0,
+            stagger: 0.5,
+            scrollTrigger: {
+                trigger: '.audience__content h1',
+            }
+        })
+    })
     return (
         <section className='audience'>
             <div className="audience__content">
-                {/* <h1>Open doors, diverse minds: welcomes all</h1> */}
                 <div className="audience__content-cards">
                     <Image src='/assets/icons/user.svg' alt='freelancers' width={24} height={24} />
-                    <p>Freelancers</p></div>
+                    <p>Freelancers</p>
+                </div>
                 <div className="audience__content-cards">
                     <Image src='/assets/icons/book.svg' alt='students' width={24} height={24} />
-                    <p>Students</p></div>
+                    <p>Students</p>
+                </div>
                 <div className="audience__content-cards">
                     <Image src='/assets/icons/monitor.svg' alt='students' width={24} height={24} />
-                    <p>Remote Workers</p></div>
+                    <p>Remote Workers</p>
+                </div>
                 <div className="audience__content-cards">
                     <Image src='/assets/icons/employee.svg' alt='students' width={24} height={24} />
-                    <p>Corporate Employees</p></div>
+                    <p>Corporate Employees</p>
+                </div>
                 <h1>Open doors, diverse minds: welcomes all</h1>
             </div>
         </section>
