86bec06b856f7bafc79ddc4148eb02be6660faf4
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-03-31 18:35:18 +0100

    Remove unused imports and commented code

--- app/ui/loader/loader.tsx
+++ app/ui/loader/loader.tsx
@@ -1,54 +1,6 @@
-"use client";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import { useGSAP } from '@gsap/react'
-import NavBar from '../nav-bar'
-// import { useEffect, useState } from 'react'
-
-gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(useGSAP)
-
-
 // export default function Loader({ isLoading = true }: { isLoading: boolean }) {
 export default function Loader() {
-
-    useGSAP(() => {
-        // gsap.to('.loader', {
-        // height: 0,
-        // duration: 1,
-        // y: 200,
-        // opacity: 0,
-        // ease: 'power2.out',
-        // delay: 3,
-        // stagger: 0.5,
-        // })
-        // gsap.to('.loader__wrapper', {
-        // display: 'none',
-        // opacity: 0,
-        // duration: 0.1
-        // })
-    })
-    // const [isLoading, setIsLoading] = useState(true);
-
-    // useEffect(() => {
-    //     const handleContentLoaded = () => {
-    //         setIsLoading(false);
-    //     };
-
-    //     document.addEventListener('DOMContentLoaded', handleContentLoaded);
-
-    //     // Cleanup function to remove event listener on unmount
-    //     return () => document.removeEventListener('DOMContentLoaded', handleContentLoaded);
-    // }, []);
-
-    // useEffect(() => {
-    //     // Simulate some loading time (replace with your actual loading logic)
-    //     setTimeout(() => {
-    //         setIsLoading(false);
-    //     }, 2000);
-    // }, []);
     return (
         <section className="loader">
             <div className="loader__wrapper">
