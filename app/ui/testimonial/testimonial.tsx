d2b9ed6747dbe44ba337982f7c9bfa107eaa8724
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-04-01 03:20:39 +0100

    Add position and z-index to testimonial section

--- app/ui/testimonial/testimonial.tsx
+++ app/ui/testimonial/testimonial.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 export default function Testimonial() {
     return (
         <section className='testimonial'>
-            {/* <div className='black-overlay'></div> */}
+            <div className="testimonial__overlay"></div>
             <div className="testimonial__text">
                 <p>“The environment at Freelancer is super inclusive. I love coming here knowing I’m going to have a fun, productive day”</p>
                 <h3>Courtney Henry</h3>
